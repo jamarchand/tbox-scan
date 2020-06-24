3eb9b5457cbdb7981110f9ed213b3605252c38b6
jamarchand <marchand@berkeley.edu>
2020-06-24 15:13:24 -0400

    added sh

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ conda activate tbdb
 
 echo "Exporting path to .bash_profile"
 
-echo "alias tbox-scan='./tbox-scan.sh'" >> ~/.bash_profile
+echo "alias tbox-scan='sh ./tbox-scan.sh'" >> ~/.bash_profile
 echo 'export PATH=${PATH}:'"$(pwd)/tbox-scan" >> ~/.bash_profile
