8bb010a1a68b495347817eab4083746bf04865b9
jamarchand <marchand@berkeley.edu>
2020-06-24 11:58:08 -0400

    update with error handling

--- tboxscnr.py
+++ tboxscnr.py
@@ -8,12 +8,13 @@ import sys
 
 
 
-infile = 'test.fa'
-outfile = 'out.csv'
+infile = sys.argv[1]
+outfile = sys.argv[2]
+cm = sys.argv[3]
 
 aalut=pd.read_csv('rccodonLUT.csv')
  
-os.system('cmsearch --notrunc --notextw RF00230.cm '+infile+' > INFERNAL.txt')
+os.system('cmsearch --notrunc --notextw '+cm+' '+infile+' > INFERNAL.txt')
 os.system('python3 pipeline_master.py INFERNAL.txt '+outfile+' '+infile+' $3 > log.txt')
 out = pd.read_csv(outfile)
 print(out)
@@ -23,7 +24,10 @@ print('\n')
 
 aalist=[None]*len(out)
 for i in range (0,len(out)):
-    aalist[i]='T-box '+aalut.loc[aalut['AC']==out['codon'].iloc[i],['AA']].values[0][0]
+    try:
+        aalist[i]='T-box '+aalut.loc[aalut['AC']==out['codon'].iloc[i],['AA']].values[0][0]
+    except:
+        aalist[i]='None' 
 
 out['AA']=aalist
 outmess=out[['AA','Tbox_start', 'Tbox_end', 'codon_region', 'codon', 'discriminator', 'Score']]
