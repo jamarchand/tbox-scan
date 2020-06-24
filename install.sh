f2094bef848c5302fbd020f8973ac5c4f97e2308
mpiersonsmela <62316569+mpiersonsmela@users.noreply.github.com>
2020-06-24 18:54:14 -0400

    don't use alias

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@
 echo "Installing dependencies"
 conda init bash
 conda env create -f environment.yml
-conda activate tbdb
+
+source activate tbdb
 
 echo "Exporting path to .bash_profile"
 
-echo "alias tbox-scan='./tbox-scan.sh'" >> ~/.bash_profile
-echo 'export PATH=${PATH}:'"$(pwd)/tbox-scan" >> ~/.bash_profile
+echo "" >> ~/.bash_profile
+echo 'export PATH=${PATH}:'"$(pwd)" >> ~/.bash_profile
+exec bash
