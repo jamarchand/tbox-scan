55f02f43810e1d27735aaeb649a92a69bf3e124d
jamarchand <marchand@berkeley.edu>
2020-06-25 07:18:27 -0400

    changes to see if this works

--- install.sh
+++ install.sh
@@ -11,4 +11,6 @@ echo "Exporting path to .bash_profile"
 
 echo "" >> ~/.bash_profile
 echo 'export PATH=${PATH}:'"$(pwd)" >> ~/.bash_profile
-exec bash
+echo "alias tbox-scan='sh tbox-scan'" >> ~/.bash_profile
+
+#exec bash
